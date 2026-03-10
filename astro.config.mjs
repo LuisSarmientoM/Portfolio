dfec242399afc6294314059587d18fedb07c4794
Luis Sarmiento <lsarmientom010@gmail.com>
2026-03-10 18:12:10 -0500

    Add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://lsarmiento.dev",
   output: "static",
   trailingSlash: "always",
+
   vite: {
       plugins: [tailwindcss()],
   },
 
+  adapter: cloudflare(),
 });
